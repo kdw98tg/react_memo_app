ccea9cc608bd35dc5ec89594d8b1bb01e8cbb6c9
kdw98tg <117254224+kdw98tg@users.noreply.github.com>
2026-01-15 17:51:05 +0900

    delete 하는 기능 문제 있음. 이거 고쳐야함

--- memoapp/src/App.js
+++ memoapp/src/App.js
@@ -24,7 +24,6 @@ function App() {
   //왼쪽 메모 리스트의 메모 아이템 선택했을 때 작동하는 기능
   const onMemoClicked = (memoIndex) => {
     setSelectedMemoIndex(memoIndex);
-    console.log(memoIndex);
   };
 
   //메모 추가 기능
@@ -55,9 +54,25 @@ function App() {
   const deleteMemo = (index) => {
     const memosCopy = [...memoArray];
     memosCopy.splice(index, 1);
-    setMemoArray(memosCopy);
     if (index === selectedMemoIndex) {
       setSelectedMemoIndex(0);
+      setMemoArray(memosCopy);
+      return 0;
+      //가장 최신의 값을 전달받을 수 있음
+      //현재 랜더링 시점의 state가 아니라, React가 보관하고 있는 가장 최신의 state를 받음
+      //이게 왜 필요하냐면, React의 setState는 즉시 값을 바꾸지 않음
+      // 여러 개가 한번에 batch 처리되어서, 한번에 바뀜
+      //같은 state에 대해 여러 번 호출 될 수 있음.
+      // setSelectedMemoIndex((state) => {
+      //   if (memosCopy.length === 0) return 0;
+      //   if (index < state) return state - 1;
+      //   if (index === state) return Math.max(0, state - 1);
+      //   return state;
+      // });
+      // (선택사항) 삭제한 메모가 선택된 메모보다 앞에 있으면 인덱스가 당겨지므로 보정
+    }
+    if (index < selectedMemoIndex) {
+      setSelectedMemoIndex(selectedMemoIndex - 1);
     }
   };
 
